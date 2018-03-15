498c6975ccf92f757f4585637f71a14243e4d35b
Ubuntu <ubuntu@ip-172-31-19-123.us-west-2.compute.internal>
2018-03-15 03:26:38 +0000

    add metrics

--- src/stragety/baseline.py
+++ src/stragety/baseline.py
@@ -1,5 +1,5 @@
 import sys, os
-from util import read_stock
+from util import read_stock, avg, ema, metrics
 
 if len(sys.argv) < 4:
     print 'Usage: python baseline.py symbol start_date:YYYYMM end_date:YYYYMM'
@@ -15,7 +15,7 @@ quotes = read_stock(root, symbol, start, end)
 close = [(x[0],x[5]) for x in quotes]
 
 def optimal(quotes, invest=1000000):
-    bank,share = invest,0
+    bank,share,assets = invest,0,[invest]
     for i in range(1,len(quotes)):
         if share > 0 and quotes[i][1] < quotes[i-1][1]:
             bank += share*quotes[i-1][1]
@@ -23,47 +23,46 @@ def optimal(quotes, invest=1000000):
         elif bank >= quotes[i-1][1] and quotes[i][1] > quotes[i-1][1]:
             share += bank/quotes[i-1][1]
             bank -= share*quotes[i-1][1]
-    return 10000*(bank+share*quotes[i][1]-invest)/invest/100.0
+        assets.append(bank+share*quotes[i][1])
+    return assets
 
-def macd_raw(quotes, invest=1000000, short=12, long=26):
-    def macd(quotes, period):
-        total = 0
-        macd = [0]*len(quotes)
-        for i in range(period):
-            total += quotes[i][1]
-        macd[i] = total/period
-        for i in range(period, len(quotes)):
-            total -= quotes[i-period][1]
-            total += quotes[i][1]
-            macd[i] = total/period
-        return macd
-    macd_short = macd(quotes, short)
-    macd_long = macd(quotes, long)
-    bank,share = invest,0
+def macd_avg(quotes, invest=1000000, short=12, long=26):
+    bank,share,assets = invest,0,[invest]*long
+    avg_short = avg([x[1] for x in quotes], short)
+    avg_long = avg([x[1] for x in quotes], long)
     for i in range(long, len(quotes)):
-        if macd_short[i] > macd_long[i] and bank >= quotes[i][1]:
+        if avg_short[i] > avg_long[i] and bank >= quotes[i][1]:
             share += bank/quotes[i][1]
             bank -= share*quotes[i][1]
-        elif macd_short[i] < macd_long[i] and share > 0:
+        elif avg_short[i] < avg_long[i] and share > 0:
             bank += share*quotes[i][1]
             share = 0
-    return 10000*(bank+share*quotes[i][1]-invest)/invest/100.0
+        assets.append(bank+share*quotes[i][1])
+    return assets
 
-def macd_std(quotes, invest=1000000, short=12, long=26, M=9):
-    def ema(quotes, period):
-        ret = []
-        for i in range(len(quotes)):
-            dema = quotes[i][1] if i==0 else (2.0*quotes[i][1]+(period-1)*ret[-1])/(period+1)
-            ret.append(dema)
-        return ret
-    ema_short = ema(quotes, short)
-    ema_long = ema(quotes, long)
-    bank,share = invest,0
+def macd_dif(quotes, invest=1000000, short=12, long=26, M=9):
+    bank,share,assets = invest,0,[]
+    ema_short = ema([x[1] for x in quotes], short)
+    ema_long = ema([x[1] for x in quotes], long)
     diff = [ema_short[i] - ema_long[i] for i in range(len(quotes))]
-    dea = []
     for i in range(len(diff)):
-        ddea = diff[i] if i==0 else (2.0*diff[i]+(M-1)*dea[-1])/(M+1)
-        dea.append(ddea)
+        macd = diff[i]
+        if macd > 0 and bank >= quotes[i][1]:
+            share += bank/quotes[i][1]
+            bank -= share*quotes[i][1]
+        elif macd < 0 and share > 0:
+            bank += share*quotes[i][1]
+            share = 0
+        assets.append(bank+share*quotes[i][1])
+    return assets
+
+def macd_dea(quotes, invest=1000000, short=12, long=26, M=9):
+    bank,share,assets = invest,0,[]
+    ema_short = ema([x[1] for x in quotes], short)
+    ema_long = ema([x[1] for x in quotes], long)
+    diff = [ema_short[i] - ema_long[i] for i in range(len(quotes))]
+    dea = ema(diff, M)
+    for i in range(len(diff)):
         macd = 2.0*(diff[i]-dea[i])
         if macd > 0 and bank >= quotes[i][1]:
             share += bank/quotes[i][1]
@@ -71,23 +70,30 @@ def macd_std(quotes, invest=1000000, short=12, long=26, M=9):
         elif macd < 0 and share > 0:
             bank += share*quotes[i][1]
             share = 0
-    return 10000*(bank+share*quotes[i][1]-invest)/invest/100.0
+        assets.append(bank+share*quotes[i][1])
+    return assets
     
-def turtle(quotes, invest=1000000, short=10, long=20):
+def turtle_raw(quotes, invest=1000000, short=10, long=20):
     def pmax(quotes, period):
         stk = []
         for i in range(period):
             pass
 
-def stupid_1(quotes, invest=1000000):
-    share = invest/quotes[0][1]
-    bank = invest-share*quotes[0][1]
-    return 10000*(bank+share*quotes[-1][1]-invest)/invest/100.0
+def print_metrics(assets, close, label):
+    print '[',label,']'
+    m = metrics(assets, [x[1] for x in close])
+    print '%20s' % 'Returns', '=>', '%.2f%%' % (100*m['returns'])
+    print '%20s' % 'Annual Returns', '=>', '%.2f%%' % (100*m['annual returns'])
+    print '%20s' % 'Benchmark Returns', '=>', '%.2f%%' % (100*m['benchmark returns'])
+    print '%20s' % 'Alpha', '=>', '%.4f' % m['alpha']
+    print '%20s' % 'Beta', '=>', '%.4f' % m['beta']
+    print '%20s' % 'Sharpe Ratio', '=>', '%.4f' % m['sharpe ratio']
+    print '%20s' % 'Volatility', '=>', '%.4f' % m['volatility']
+    print '%20s' % 'Information Ratio', '=>', '%.4f' % m['information ratio']
+    print '%20s' % 'Max Drawdown', '=>', '%.2f%%' % (100*m['max drawdown'])
+    print ''
 
-def stupid_2(quotes, invest=1000000):
-    pass
-
-print 'Optimal Performance:',optimal(close),'%'
-print 'MACD RAW Performance:',macd_raw(close),'%'
-print 'MACD STD Performance:',macd_std(close),'%'
-print 'Stupid #1 Performance:',stupid_1(close),'%'
+print_metrics(optimal(close), close, 'Optimal')
+print_metrics(macd_avg(close), close, 'MACD AVG')
+print_metrics(macd_dif(close), close, 'MACD DIF')
+print_metrics(macd_dea(close), close, 'MACD DEA')
--- src/stragety/util.py
+++ src/stragety/util.py
@@ -18,6 +18,74 @@ def read_stock(root, symbol, start, end):
     ret.sort(key=lambda x:x[0]) 
     return ret
 
+def avg(quotes, period):
+    total = 0.0
+    ret = [0]*len(quotes)
+    for i in range(period):
+        total += quotes[i]
+    ret[i] = total/period
+    for i in range(period, len(quotes)):
+        total -= quotes[i-period]
+        total += quotes[i]
+        ret[i] = total/period
+    return ret
+
+def ema(quotes, period):
+    ret = []
+    for i in range(len(quotes)):
+        if i==0:
+            val = quotes[i]
+        else:
+            val = (2.0*quotes[i]+(period-1)*ret[-1])/(period+1)
+        ret.append(val)
+    return ret
+
+def mean(arr):
+    return float(sum(arr)) / max(len(arr), 1)
+
+def diff_mean(arr):
+    m = mean(arr)
+    return [x-m for x in arr]
+
+def dot(arr1, arr2):
+    return sum([arr1[i]*arr2[i] for i in range(len(arr1))])
+
+def cov(arr1, arr2):
+    return dot(diff_mean(arr1), diff_mean(arr2)) / (len(arr1)-1)
+
+def var(arr):
+    m = mean(arr)
+    return sum([(x-m)**2 for x in arr]) / (len(arr)-1)
+
+def std(arr):
+    return var(arr)**0.5
+
+def metrics(assets, quotes):
+    if len(assets) != len(quotes):
+        return
+    N = len(assets)
+    ret = {}
+    ret['safe returns'] = 0.038
+    ret['returns'] = float(assets[-1])/assets[0]-1
+    ret['annual returns'] = ret['returns']/N*250
+    ret['benchmark returns'] = (float(quotes[-1])/quotes[0]-1)/N*250
+    ret['max drawdown'] = 0
+    strategy, benchmark = [], []
+    max_assets = 0
+    for i in range(1,N):
+        strategy.append(float(assets[i])/assets[i-1]-1)
+        benchmark.append(float(quotes[i])/quotes[i-1]-1)
+        max_assets = max(max_assets, assets[i])
+        ret['max drawdown'] = max(ret['max drawdown'], 1-float(assets[i])/max_assets)
+    ret['beta'] = cov(strategy, benchmark) / var(benchmark)
+    ret['alpha'] = (ret['annual returns']-ret['safe returns']) - ret['beta']*(ret['benchmark returns']-ret['safe returns'])
+    ret['volatility'] = 250 * std(strategy)
+    ret['sharpe ratio'] = (ret['annual returns']-ret['safe returns'])/ret['volatility']
+    diff = [strategy[i]-benchmark[i] for i in range(len(strategy))]
+    ret['information ratio'] = mean(diff) / std(diff)
+    
+    return ret
+
 if __name__ == '__main__':
     print read_csv('/home/ubuntu/kf-stock/data/AAPL/201801.csv')
     print read_stock('/home/ubuntu/kf-stock/data', 'AAPL', '201801','201802')
